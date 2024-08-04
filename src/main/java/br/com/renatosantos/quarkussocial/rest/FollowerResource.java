835773bb7d4193a82632e1a2cd2957c57285b194
Renato Santos <renato.santos@reserve.com.br>
2024-08-04 19:28:43 -0300

    master - adicionada javadocs para followerresource

--- src/main/java/br/com/renatosantos/quarkussocial/rest/FollowerResource.java
+++ src/main/java/br/com/renatosantos/quarkussocial/rest/FollowerResource.java
@@ -9,13 +9,20 @@ import br.com.renatosantos.quarkussocial.rest.dto.FollowerResponse;
 import br.com.renatosantos.quarkussocial.rest.dto.FollowersPerUserResponse;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
-import jakarta.validation.Valid;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.stream.Collectors;
+
+/**
+ * This class represents the RESTful resource for managing followers.
+ * It handles HTTP requests related to following and unfollowing users.
+ *
+ * @author Renato
+ * @since 1.0.0
+ */
 
 @Path("/users/{userId}/followers")
 @Slf4j
@@ -25,6 +32,14 @@ public class FollowerResource {
     private final FollowerRepository followerRepository;
     private final UserRepository userRepository;
 
+    /**
+     * Constructor for the FollowerResource class.
+     * It injects the required repositories using CDI.
+     *
+     * @param followerRepository The FollowerRepository instance
+     * @param userRepository The UserRepository instance
+     */
+
     @Inject
     public FollowerResource(FollowerRepository followerRepository,
                             UserRepository userRepository) {
@@ -32,6 +47,18 @@ public class FollowerResource {
         this.userRepository = userRepository;
     }
 
+    /**
+     * Handles HTTP PUT requests to follow a user.
+     * It checks if the user and follower exist, and if the user is not following the follower.
+     * If all conditions are met, it creates a new Follower entity and persists it in the database.
+     *
+     * @param userId The ID of the user who wants to follow
+     * @param followerRequest The request containing the ID of the follower
+     * @return A Response with status 200 and a message indicating the successful follow
+     * @throws NullPointerException If the user or follower does not exist
+     * @throws SocialMediaExceptions.SelfFollowException If the user tries to follow themselves
+     * @throws SocialMediaExceptions.AlreadyFollowedException If the user is already following the follower
+     */
     @PUT
     @Transactional
     public Response followUser(@PathParam("userId") Long userId,
@@ -62,6 +89,16 @@ public class FollowerResource {
         }
     }
 
+    /**
+     * Handles HTTP GET requests to list the followers of a user.
+     * It retrieves the list of followers from the database and returns them in a response.
+     *
+     * @param userId The ID of the user whose followers are to be listed
+     * @return A Response with status 200 and the list of followers
+     * @return A Response with status 404 If the user does not have any
+     * followers
+     * @throws NullPointerException If the user does not exist
+     */
     @GET
 public Response listFollowers(@PathParam("userId") Long userId){
     var user = userRepository.findById(userId);
@@ -87,6 +124,18 @@ public Response listFollowers(@PathParam("userId") Long userId){
     responseObject.setContent(followerList);
     return Response.ok(responseObject).build();
 }
+    /**
+     * Handles HTTP DELETE requests to unfollow a user.
+     * It checks if the user and follower exist, and if the user is following the follower.
+     * If all conditions are met, it deletes the Follower entity from the database.
+     *
+     * @param userId The ID of the user who wants to unfollow
+     * @param followerId The ID of the follower to be unfollowed
+     * @return A Response with status 200 and a message indicating the successful unfollow
+     * @throws NullPointerException If the user or follower does not exist
+     * @throws SocialMediaExceptions.NotFollowingException If the user is not following the follower
+     * @throws IllegalStateException If the follower relationship does not exist in the database
+     */
     @DELETE
     @Transactional
     public Response unfollowUser(@PathParam("userId") Long userId,
