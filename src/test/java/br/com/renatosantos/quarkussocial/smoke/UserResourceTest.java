4d1adf6024203184a47789b76e545c3033c58b83
Renato Santos <renato.santos@reserve.com.br>
2024-08-06 19:51:32 -0300

    Master - Finalizados testes de operações de u suário

--- src/test/java/br/com/renatosantos/quarkussocial/smoke/UserResourceTest.java
+++ src/test/java/br/com/renatosantos/quarkussocial/smoke/UserResourceTest.java
@@ -1,13 +1,15 @@
 package br.com.renatosantos.quarkussocial.smoke;
 
 import br.com.renatosantos.quarkussocial.rest.dto.CreateUserRequest;
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+import java.lang.reflect.Array;
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
@@ -15,9 +17,14 @@ import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserResourceTest {
 
+    @TestHTTPResource("/users")
+    URL apiURL;
+
     @Test
+    @Order(0)
     @DisplayName("Should create an user successfully")
     public void createUserTest(){
         var user = new CreateUserRequest();
@@ -28,7 +35,7 @@ class UserResourceTest {
                 .contentType(ContentType.JSON)
                 .body(user)
                 .when()
-                .post("/users")
+                .post(apiURL)
                 .then()
                 .extract()
                 .response();
@@ -38,7 +45,8 @@ class UserResourceTest {
     }
 
     @Test
-    @DisplayName("Should return error when json is not valid")
+    @Order(1)
+    @DisplayName("Should thrown Age and Name violations")
     public void createUserValidationErrorTest(){
         var user = new CreateUserRequest();
         user.setName(null);
@@ -47,12 +55,13 @@ class UserResourceTest {
                 .contentType(ContentType.JSON)
                 .body(user)
                 .when()
-                .post("/users")
+                .post(apiURL)
                 .then()
                 .extract()
                 .response();
 
-        boolean expectedViolationFound = false;
+        boolean nameViolationFound = false;
+        boolean ageViolationFound = false;
         List<Map<String,Object>> violations =
                 JsonPath.from(response.asString()).getList("violations");
         if(violations != null){
@@ -60,18 +69,42 @@ class UserResourceTest {
         for (Map<String,Object> violation : violations) {
             String field = (String) violation.get("field");
             String message = (String) violation.get("message");
-            if ("name".contains(field) && "Name is required".equals(message)){
-                expectedViolationFound = true;
-            assertTrue(expectedViolationFound, "Expected violation found for 'name' with message 'Name is required'");
-            assertEquals(400,response.statusCode());
-            break;
-        }}
+            if (field.contains("name") && message.equals("Name is required")){
+                nameViolationFound = true;
+                }
+            else if (field.contains("age") && message.equals("Age is required")){
+                ageViolationFound = true;
 
+            }
+            assertEquals(400,response.statusCode());
+        }
+            assertTrue(nameViolationFound, "Expected name violation found");
+            assertTrue(ageViolationFound, "Expected age violation found");
 
     }
-        else {
-            fail("Violation not found for 'name' with message 'Name is " +
-                    "required'");
+}
 
-        }
-}}
+    @Test
+    @Order(2)
+    @DisplayName("Should return a list of users")
+    public void getUsersTest(){
+        Response response = given()
+               .when()
+               .get(apiURL)
+               .then()
+               .extract()
+               .response();
+        System.out.println(response);
+        assertEquals(200, response.statusCode());
+        assertNotNull(response.body());
+
+        List<Map<String, Object>> users = response.jsonPath().getList("");
+        assertNotNull(users);
+        assertTrue(users.size() > 0, "Expected at least one user in the list");
+
+        // Additional assertions for specific user properties if needed
+        Map<String, Object> firstUser = users.get(0);
+        assertEquals("Fulaninho", firstUser.get("name"));
+        assertEquals(20, firstUser.get("age"));
+    }
+}
